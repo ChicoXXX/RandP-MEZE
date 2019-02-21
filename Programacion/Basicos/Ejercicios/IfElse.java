66bec088ec2f7e72611f9a7c773d81269f3e861c
ChicoXXX <osvaldo@cryptolab.net>
2019-02-21 15:49:31 -0600

    Uso de If-Else en java.

---
+++ Programacion/Basicos/Ejercicios/IfElse.java
@@ -0,0 +1,16 @@
+
+// Programa para mostrar el uso de if-else
+public class IfElse
+{
+    public static void main(String args[])
+    {
+        int i = 15;
+
+        if (i < 15)
+	    {
+               System.out.println("i es menor que 15");
+            }
+        else
+            System.out.println("i es mayor que 15");
+    }
+}
