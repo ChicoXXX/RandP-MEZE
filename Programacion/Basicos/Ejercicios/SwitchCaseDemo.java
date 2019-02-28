dccf567a5c9e6206643e1a4ecaf8e596a37f2bad
ChicoXXX <osvaldo@cryptolab.net>
2019-02-27 22:13:36 -0600

    Ejemplo de Switch-Case.

---
+++ Programacion/Basicos/Ejercicios/SwitchCaseDemo.java
@@ -0,0 +1,24 @@
+
+// Ejemplo de uso de switch-case en java
+
+class SwitchCaseDemo
+{
+    public static void main(String args[])
+    {
+        int i = 1;
+        switch (i)
+        {
+        case 0:
+            System.out.println("i vale cero.");
+            break;
+        case 1:
+            System.out.println("i vale uno.");
+            break;
+        case 2:
+            System.out.println("i vale dos.");
+            break;
+        default:
+            System.out.println("i es mayor que dos.");
+        }
+    }
+}
