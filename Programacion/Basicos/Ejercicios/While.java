f197f9a861d9c7457f3359d4ec3a0fa4c771bbd5
ChicoXXX <osvaldo@cryptolab.net>
2019-02-27 22:14:45 -0600

    Ejemplo de While en java.

---
+++ Programacion/Basicos/Ejercicios/While.java
@@ -0,0 +1,16 @@
+
+// Programa para mostrar el uso de While
+
+public class While
+{
+    public static void main(String args[])
+    {
+        int variable = 0;
+        System.out.println(variable);
+        while( variable < 10 ){
+            variable = variable + 1;
+            System.out.println(variable);
+        }
+        System.out.println(variable);
+    }
+}
