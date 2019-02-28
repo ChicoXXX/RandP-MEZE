3b35dfaf009d39fb8f4cfccd461bc16d13f9f50b
ChicoXXX <osvaldo@cryptolab.net>
2019-02-27 22:11:16 -0600

    Ejemplo de Do-While.

---
+++ Programacion/Basicos/Ejercicios/DoWhile.java
@@ -0,0 +1,16 @@
+
+// Programa para mostrar el uso de Do-While
+public class DoWhile
+{
+    public static void main(String args[])
+    {
+        int variable = 0;
+        System.out.println(variable);
+        do {
+            variable = variable + 1;
+            System.out.println(variable);
+        } while( variable == 1 );
+        System.out.println(variable);
+    }
+}
+
